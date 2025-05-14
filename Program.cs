33d68bbf1003bc1f65d307f0a97b5923988ef0a3
MotiWolff <motiwolff@gmail.com>
2025-05-14 12:05:20 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello people!");
+        }
+    }
+}
