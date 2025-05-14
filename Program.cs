7da7adfd0ffc29aa359e9c996585f8de3cc4d397
MotiWolff <motiwolff@gmail.com>
2025-05-14 12:20:42 +0300

    Moti change

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello people!");
+            Console.WriteLine("This was printed by the other branch!");
         }
     }
 }
